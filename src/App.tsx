0c497d21035431a6565107f8fd604f0780576532
lexbal <alexballe35@gmail.com>
2022-05-27 01:34:43 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import {
 } from "react-router-dom";
 import Header from './components/Page/Header/Header';
 import Container from './components/Common/Container/Container';
-import Footer from './components/Page/Footer/Footer';
 import Home from './components/Page/Home/Home';
 import './App.scss';
+import Footer from './components/Page/Footer/Footer';
 
 function App() {
   return (
--- src/components/Page/Presentation/Presentation.tsx
+++ src/components/Page/Presentation/Presentation.tsx
@@ -1,21 +1,34 @@
 import React, { FC } from 'react';
-import SocialMedia from '../../Common/SocialMedia/SocialMedia';
-import { faGitAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import ScrollAnimation from 'react-animate-on-scroll';
+import unDrawImage from '../../../assets/undraw_developer_activity_re_39tg.svg';
+import Button from '../../Common/Button/Button';
 import './Presentation.scss';
 
 interface PresentationProps {}
 
 const Presentation: FC<PresentationProps> = () => (
   <section className="Presentation page-bloc" id="Presentation" data-testid="Presentation">
-    <h5>Bonjour, je m'appelle Alexandre 👋</h5>
-    <h1>Fullstack Developer</h1>
-    <div className="description">
-      J'aime construire des applications web et mobile.
-    </div>
-    <div className="social-media">
-      <SocialMedia icon={faGitAlt} href="https://github.com/lexbal" />
-      <SocialMedia icon={faLinkedinIn} href="https://www.linkedin.com/in/alexandre-balle-199b36161/?originalSubdomain=fr" />
-    </div>
+    <div className="text-left">
+        <ScrollAnimation animateIn="fadeInUp">
+          <p>Bonjour, je m'appelle</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.2*1000}>
+          <h1>Balle Alexandre</h1>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
+          <h3>Developpeur Fullstack</h3>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
+          <p className="small-resume">J'aime construire des applications web et mobile.</p>
+        </ScrollAnimation>
+
+        <Button type="custom" href="#Contact">Contacter moi</Button>
+      </div>
+      <div className="image-right">
+        <ScrollAnimation animateIn="fadeInRight" delay={1*1000}>
+          <img src={unDrawImage} alt="unDrawImage"/>
+        </ScrollAnimation>
+      </div>
   </section>
 );
 
