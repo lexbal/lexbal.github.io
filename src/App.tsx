546e8be7bad7fa234b4a86e489db99b10e3ae7c3
lexbal <alexballe35@gmail.com>
2022-05-27 19:00:05 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route, Routes
-} from "react-router-dom";
 import Header from './components/Page/Header/Header';
 import Container from './components/Common/Container/Container';
 import Home from './components/Page/Home/Home';
@@ -12,13 +8,11 @@ import Footer from './components/Page/Footer/Footer';
 function App() {
   return (
     <div className="App" data-testid="App">
-      <Router>
-        <Header />
-        <Container>
-          <Home />
-        </Container>
-        <Footer />
-      </Router>
+      <Header />
+      <Container>
+        <Home />
+      </Container>
+      <Footer />
     </div>
   );
 }
